bee8b32b90344d970a005462c422487584e80cab
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-09-08 19:16:48 -0700

    save button now saves text area and ID

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
+//setting date format to variable displaying that variable
 var localDate = moment().format('dddd, MMM Do YYYY');
 $('#currentDay').html(localDate);
+
+// Event Listener to save
+$(".saveBtn").on('click', function(){
+    //Grabs text from textarea and the ID with its time slot
+    var descriptionText = $(this).siblings('.description').val();
+    var timePlace = $(this).parent().attr('id');
+    //store with localstorage console log to debug
+    localStorage.setItem(descriptionText, timePlace);
+    console.log(descriptionText)
+    console.log(timePlace)    
+})
+
+function timeColor() {
+    // get current hour
+    var hourIs = moment().hour();
+}
